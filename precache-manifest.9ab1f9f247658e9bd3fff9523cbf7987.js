c7620f29e071f0158c6bcff1e07e41faef50c12c
Jacob Wilson <jacob@jacobwilson.net>
2022-08-31 16:28:55 -0400

    first upload

---
+++ precache-manifest.9ab1f9f247658e9bd3fff9523cbf7987.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7eb39e9e68ffeecab651d607df10a98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9970b5fb52c16383b6bf",
+    "url": "/static/css/2.453240a7.chunk.css"
+  },
+  {
+    "revision": "3d70e3883dd1bf7c124b",
+    "url": "/static/css/main.a2b32ab5.chunk.css"
+  },
+  {
+    "revision": "9970b5fb52c16383b6bf",
+    "url": "/static/js/2.2ae8480b.chunk.js"
+  },
+  {
+    "revision": "d3e49d62c5e83bd760c7639e00b15fb0",
+    "url": "/static/js/2.2ae8480b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "731cb168cfd02d6fb3fd",
+    "url": "/static/js/3.28245d1b.chunk.js"
+  },
+  {
+    "revision": "3d70e3883dd1bf7c124b",
+    "url": "/static/js/main.bc2e694b.chunk.js"
+  },
+  {
+    "revision": "b4328641e941d35aa5db",
+    "url": "/static/js/runtime~main.9209eaf7.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
